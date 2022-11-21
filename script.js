92a824baabc5850eab9a2b0ec14a8fefa39cee77
Harshpreet Singh Bhangu <102208083+Harshpreet-57@users.noreply.github.com>
2022-11-21 12:36:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch('https://api.chucknorris.io/jokes/random')
-      .then(data => data.json)
+      .then(data => data.json())
       .then(jokeData => {
         const JokeText = jokeData.value;
         const jokeElement = document.getElementById('jokeElement');
